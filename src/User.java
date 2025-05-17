6212f63e80978cd7fd7394caefa4075ae5ec6144
natalibaidoshvili12 <nbaidoshvili12@gmail.com>
2025-05-17 12:14:11 +0400

    Implement User

--- src/User.java
+++ src/User.java
@@ -1,2 +1,40 @@
 public class User {
+
+    private String username;
+    private String password;
+    private String pin;
+
+    public User() {
+
+    }
+
+    public User(String username, String password, String pin) {
+        this.username = username;
+        this.password = password;
+        this.pin = pin;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
 }
